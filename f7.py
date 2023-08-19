8affdd2e5b5a33c8cad3ff4a9d20bc450e7639c9
Muxlisa2007 <muxlisa200707@gmail.com>
2023-08-19 15:43:16 +0500

    7-masala

---
+++ f7.py
@@ -0,0 +1,25 @@
+list = [1,2,3,4,5,6,7,8,9]
+i = 0
+k = 2
+while i < len(list) :
+    if list[i] % k == 0:
+       print(list[i])
+    i += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''k = 2
+arr = [1,2,3,4,5,6,7,8,9,10]
+for i in arr:
+    if i % k == 0:
+      print(i)'''
